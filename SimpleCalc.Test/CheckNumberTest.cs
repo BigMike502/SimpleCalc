f628a48c50c4bcd9c16cd77c4ffe11988af632d9
Michael <mmelhorndesign@gmail.com>
2022-11-18 05:53:32 -0500

    Updated SimpleCalc.Test

--- SimpleCalc.Test/CheckNumberTest.cs
+++ SimpleCalc.Test/CheckNumberTest.cs
@@ -10,16 +10,31 @@ namespace SimpleCalc.Tests
     {
       //arrange
       bool testNumber;
+      bool testOporator;
+      bool testYesOrNo1;
+      bool testYesOrNo2;
+      bool testYesOrNo3;
+      bool testYesOrNo4;
 
 
       //act
 
       testNumber = InfoCheck.IsInputANumber("2");
+      testOporator = InfoCheck.CheckOperatorInput("1");
+      testYesOrNo1 = InfoCheck.CheckYesOrNo("yes");
+      testYesOrNo2 = InfoCheck.CheckYesOrNo("no");
+      testYesOrNo3 = InfoCheck.CheckYesOrNo("y");
+      testYesOrNo4 = InfoCheck.CheckYesOrNo("n");
 
 
       //assert
 
       Assert.True(testNumber);
+      Assert.True(testOporator);
+      Assert.True(testYesOrNo1);
+      Assert.True(testYesOrNo2);
+      Assert.True(testYesOrNo3);
+      Assert.True(testYesOrNo4);
 
 
     }
